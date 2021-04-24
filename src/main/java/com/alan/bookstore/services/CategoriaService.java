164e8708a720a7f4f292d43a20b41bac360473dd
alansilv <alansilv@gmail.com>
2021-04-24 09:52:14 -0300

    findAll para Categoria

--- src/main/java/com/alan/bookstore/services/CategoriaService.java
+++ src/main/java/com/alan/bookstore/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.alan.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = this.repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll() {
+		return repository.findAll();
 	}
 
 }
